a4da46f17269954aef51c1cc7d7756d3d4a54ab3
Kaveh Kiani <kavehkiani783@gmail.com>
2026-03-15 22:52:42 +0000

    Update run_tmr_v2_first.py

--- run_tmr_v2_first.py
+++ run_tmr_v2_first.py
@@ -1,60 +1,60 @@
 import subprocess
 
 runs = [
-    # TMR v1 ListOps
+    # NUBNET v1 ListOps
     [
         "python", "main.py",
         "--dataset", "listops_synth",
-        "--model", "tmr",
+        "--model", "NubNet",
         "--epochs", "3",
         "--batch_size", "64",
         "--max_len", "512",
         "--seed", "42",
         "--d_model", "128",
-        "--tmr_slots", "32",
-        "--tmr_steps", "1",
+        "--NubNet_slots", "32",
+        "--NubNet_steps", "1",
     ],
 
-    # TMR v2 ListOps
+    # NUBNET v2 ListOps
     [
         "python", "main.py",
         "--dataset", "listops_synth",
-        "--model", "tmr_v2",
+        "--model", "NubNet_v2",
         "--epochs", "3",
         "--batch_size", "64",
         "--max_len", "512",
         "--seed", "42",
         "--d_model", "128",
-        "--tmr_slots", "32",
-        "--tmr_steps", "1",
+        "--NubNet_slots", "32",
+        "--NubNet_steps", "1",
     ],
 
-    # TMR v1 IMDB
+    # NUBNET v1 IMDB
     [
         "python", "main.py",
         "--dataset", "imdb",
-        "--model", "tmr",
+        "--model", "NubNet",
         "--epochs", "3",
         "--batch_size", "64",
         "--max_len", "512",
         "--seed", "42",
         "--d_model", "128",
-        "--tmr_slots", "32",
-        "--tmr_steps", "1",
+        "--NubNet_slots", "32",
+        "--NubNet_steps", "1",
     ],
 
-    # TMR v2 IMDB
+    # NUBNET v2 IMDB
     [
         "python", "main.py",
         "--dataset", "imdb",
-        "--model", "tmr_v2",
+        "--model", "NubNet_v2",
         "--epochs", "3",
         "--batch_size", "64",
         "--max_len", "512",
         "--seed", "42",
         "--d_model", "128",
-        "--tmr_slots", "32",
-        "--tmr_steps", "1",
+        "--NubNet_slots", "32",
+        "--NubNet_steps", "1",
     ],
 ]
 
