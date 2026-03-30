f476abb8a709a5dad9c505c1e372721111e212a5
Kaveh Kiani <kavehkiani783@gmail.com>
2026-03-30 20:51:32 +0100

    Delete json_finder.py

--- json_finder.py
+++
@@ -1,8 +0,0 @@
-import json
-
-path = "results/tmr_v2_listops_synth_seed42_steps4_slots32_topk0_gate1.json"
-with open(path, "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-print(data.keys())
-print(data)
